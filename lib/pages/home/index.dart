2e9f4eee987728f8f20a7fb2715f97b045dea4ca
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-02-12 12:38:27 +0530

    Custom Tab Bar added

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:wheresmy/widgets/custom.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,9 +9,48 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(
+      length: 3,
+      vsync: this,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: customAppBar(
+        context: context,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            customTab(
+              leading: SvgPicture.asset(
+                "assets/logos/all.svg",
+              ),
+              title: "All Devices",
+            ),
+            customTab(
+              leading: SvgPicture.asset(
+                "assets/logos/mobile.svg",
+              ),
+              title: "Mobiles",
+            ),
+            customTab(
+              leading: SvgPicture.asset(
+                "assets/logos/pc.svg",
+              ),
+              title: "PCs",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
